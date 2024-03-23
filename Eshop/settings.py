569ee0563e79a851213869a60c91e6df30c5cae6
shivanjali_1012 <shindeshivanjali1213@gmail.com>
2024-03-24 05:01:45 +0530

    push

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
+import django_heroku
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +124,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "/image/download/"
 MEDIA_ROOT = BASE_DIR
+django_heroku.settings(locals())
